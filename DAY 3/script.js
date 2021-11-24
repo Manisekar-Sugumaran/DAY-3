a391ed880f3660097e616f066e841a0f7f22a76e
Manisekar <Mani.bio.197@gmail.com>
2021-11-24 10:09:24 +0530

    First commit

---
+++ DAY 3/script.js
@@ -0,0 +1,19 @@
+var resume={
+    my_details:{
+    name:"Manisekar Sugumaran",
+    email:"mani.bio.197@gmail.com",
+    mobile_no:"7338975871",
+    Address:"Karaikudi,Sivgangai",
+    pincode:"630303",
+    },
+    education:{    
+    qualification:"BE.,Mechanical Engineering",
+    college:"S.A.Engineering College",
+    Passed_out:"2021",
+    Percentage :"75%", 
+    }
+   }
+ 
+console.log(resume);
+
+
